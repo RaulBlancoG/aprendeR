acc5d8bf32c32e6622f4e247f52bb99eada592fe
raul blanco <raul blanco>
2017-02-21 13:04:59 -0600

    primera tarea

---
+++ alumnos/RaulBlancoG/tarea_1.R
@@ -0,0 +1,32 @@
+# Tarea de Pepin Bombin
+lst1 <- list(1,2.3,c(1:10))
+lst1
+lapply(lst1, mean)
+lst1 <- list(1,NA,4,NA,7,NA)
+is.na(lst1)
+funcion<-funcion(lst1){
+if(lst1!=NA){
+  x=lst1+acum
+  x=acum
+  
+}
+  else{
+    NULL
+  }
+  
+funcion
+ FRACASE
+ 
+ mat<- matrix(letters[1:25],5)
+ mat
+ 
+ colnames(mat)<-c(1:4,'col')
+ mat
+ 
+ grep("g",mat)
+ grep("s",mat)
+ grep("",mat)
+ grep(2,mat)
+ mat[[1]]
+mat['col']
+mat[,'col']
